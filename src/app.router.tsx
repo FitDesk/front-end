d6a40bd46e99e00dda4ef8204e7f918ed8e271e9
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-25 18:54:26 -0500

    feat: add Home and About components with routing setup

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -1,14 +1,22 @@
 import { Suspense } from "react";
 import { createBrowserRouter, Navigate } from "react-router";
+import Home from "./Home";
+import About from "./About";
 
 export const appRouter = createBrowserRouter([
     //Main
     {
         path: "/",
+        element: (
+            <Home />
+        )
         // element:
         // children:[
         //     {index:true , element}
         // ]
+    }, {
+        path: "/about",
+        element: <About />
     },
     {
         path: '*',
