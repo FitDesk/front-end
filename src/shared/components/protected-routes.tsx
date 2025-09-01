3362bd0e5aeae391705b998b7a8d34b1db10b1a1
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-31 21:47:36 -0500

    feat: add zustand for state management, implement authentication routes, and create currency formatter

---
+++ src/shared/components/protected-routes.tsx
@@ -0,0 +1,46 @@
+import { useAuthStore } from "@/core/store/auth.store"
+import type { PropsWithChildren } from "react"
+import { Navigate } from "react-router";
+
+export const AuthenticatedRoute = ({ children }: PropsWithChildren) => {
+    const { authStatus } = useAuthStore()
+
+    if (authStatus === 'checking') return null;
+
+    if (authStatus === 'not-authenticated') return <Navigate to={"/"} />
+
+    return children;
+
+}
+export const NotAuthenticatedRoute = ({ children }: PropsWithChildren) => {
+    const { authStatus } = useAuthStore();
+    if (authStatus === 'checking') return null;
+
+    if (authStatus === 'authenticated') return <Navigate to="/" />;
+
+    return children;
+};
+
+export const AdminRoute = ({ children }: PropsWithChildren) => {
+    const { authStatus, isAdmin } = useAuthStore();
+
+    if (authStatus === 'checking') return null;
+
+    if (authStatus === 'not-authenticated') return <Navigate to="/auth/login" />;
+
+    if (!isAdmin()) return <Navigate to="/" />;
+
+    return children;
+};
+
+export const TrainerRoute = ({ children }: PropsWithChildren) => {
+    const { authStatus, isTrainer } = useAuthStore();
+
+    if (authStatus === 'checking') return null;
+
+    if (authStatus === 'not-authenticated') return <Navigate to="/auth/login" />;
+
+    if (!isTrainer()) return <Navigate to="/" />;
+
+    return children;
+};
