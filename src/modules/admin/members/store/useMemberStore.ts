15728ea2c22789210dc30a56564ecdaf0e7a91de
Raydberg Gabriel <raydbergg@gmail.com>
2025-09-21 17:00:22 -0500

    update

--- src/modules/admin/members/store/useMemberStore.ts
+++ src/modules/admin/members/store/useMemberStore.ts
@@ -109,6 +109,12 @@ export const useMemberStore = create<MemberState>()(
         }));
       },
       
+
+
+
+
+      
+
       deleteMember: async (memberId: string) => {
         try {
           
