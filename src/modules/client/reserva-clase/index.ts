7306630289c0911e3d25af45b81db2fc7817d530
Jhuanca2023 <josehuanca612@gmail.com>
2025-10-12 19:20:31 -0500

    Reservar clase complet

---
+++ src/modules/client/reserva-clase/index.ts
@@ -0,0 +1,3 @@
+export { default as ReservaClasePage } from './pages/reserva-clase-page';
+export { ClassesService } from './services/classes.service';
+export * from './hooks/useClasses';
---
+++ src/modules/client/sesiones-personalizadas/index.ts
@@ -0,0 +1,1 @@
+export { default as SesionesPersonalizadasPage } from './pages/sesiones-personalizadas-page';
