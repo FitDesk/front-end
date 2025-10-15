e5f456c03bba4af7e3efc039ee3afe6aead6f96f
Raydberg Gabriel <raydbergg@gmail.com>
2025-10-14 22:25:08 -0500

    feat: refactor MembersPage to use new query hooks and improve filtering
    - Replaced useMembers and useMemberStore with useAllMembersQuery for data fetching.
    - Introduced useDebounce for search term handling.
    - Updated filters to include membership status and pagination.
    - Enhanced the UI with improved search and filter components.
    - Added loading and error states for better user experience.
    - Implemented a skeleton loader for the members table.
    - Created utility functions for membership status handling.
    - Added prefetching for members in the routing logic.

---
+++ src/core/routes/usePrefetchRoutes.ts
@@ -0,0 +1,62 @@
+/** biome-ignore-all lint/correctness/noUnusedVariables: <explanation> */
+import { useQueryClient } from "@tanstack/react-query";
+import type { MemberFilters } from "../interfaces/member.interface";
+import { memberKeys } from "../queries/useMemberQuery";
+import { MemberService } from "../services/member.service";
+
+export const usePrefetchRoutes = () => {
+    const queryClient = useQueryClient();
+
+
+    const prefetchMembers = () => {
+        const defaultFilters: MemberFilters = {
+            // search: null,
+            // membershipStatus: null,
+            page: 0,
+            size: 10,
+            sortField: "firstName",
+            sortDirection: "asc"
+        }
+
+        queryClient.prefetchQuery({
+            queryKey: memberKeys.list(defaultFilters),
+            queryFn: () => MemberService.getAllMembers(defaultFilters),
+            staleTime: 5 * 60 * 1000,
+
+        })
+    }
+
+
+    const prefetchTrainers = () => {
+        // Implementa el prefetch de trainers cuando tengas el servicio
+    };
+
+    const prefetchPlans = () => {
+        // Implementa el prefetch de planes
+    };
+
+    const prefetchClasses = () => {
+        // Implementa el prefetch de clases
+    };
+
+    const prefetchLocations = () => {
+        // Implementa el prefetch de ubicaciones
+    };
+
+    const prefetchBilling = () => {
+        // Implementa el prefetch de facturación
+    };
+
+    const prefetchRoles = () => {
+        // Implementa el prefetch de roles
+    };
+    return {
+        prefetchMembers,
+        prefetchTrainers,
+        prefetchPlans,
+        prefetchClasses,
+        prefetchLocations,
+        prefetchBilling,
+        prefetchRoles,
+    };
+}
